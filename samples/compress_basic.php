0dcf0c1b83fd20c52fcfd76b2328ab02b81124a4
maztch <maztch@gmail.com>
2021-10-29 16:46:10 +0200

    sample issue

--- samples/compress_basic.php
+++ samples/compress_basic.php
@@ -4,12 +4,12 @@ require_once('../vendor/autoload.php');
 //if manual installation has been used comment line that requires the autoload and uncomment this line:
 //require_once('../init.php');
 
-use Iloveimg\CompressimageImageTask;
+use Iloveimg\CompressImageTask;
 
 
 // you can call task class directly
 // to get your key pair, please visit https://developer.iloveimg.com/user/projects
-$myTask = new CompressimageImageTask('project_public_id','project_secret_key');
+$myTask = new CompresImageTask('project_public_id','project_secret_key');
 
 // file var keeps info about server file id, name...
 // it can be used latter to cancel file
